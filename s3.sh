39c2dd6e6ff88dbd6247fe57cd2df1bcba9f1c50
will <willyjiemon@yahoo.fr>
2025-07-14 10:29:37 +0200

    Mise à jour des commentaires

--- s3.sh
+++ s3.sh
@@ -10,20 +10,20 @@ ARCHIVE_NAME="archive_$DATE.tar.gz"
 DOC_ARCHIVE="$ARCHIVE/$ARCHIVE_NAME"
 ARCHIVE_ENCRYPTED="$DOC_ARCHIVE.gpg"
 GPG_PASSWORD="TonMotDePasseFort" # Usage réel mettre un mot de passe qui rempli les condition (Au moins 8 caractères melangés avec des chifrres et des caractères spéciaux) 
-s3_BUCKET="s3://njftest237"
+s3_BUCKET="s3://le_nom_de_votre_bucket"
 
-# Fonction de log
+# ========== Fonction de log ==========
 log() {
 	echo "[$(date '+%Y-%m-%d_%H-%M-%S')] $1" >> "$LOG_FILE"
 }
 
-# Vérifie si le dossier source existe
+# ========== Vérifie si le dossier source existe ==========
 if [ ! -d "$DOC_SOURCE" ]; then
         log "Le dossier source n'existe" 
         exit 1
 fi
 
-# Création de l'archive
+# ========== Création de l'archive ==========
 tar -czf "$DOC_ARCHIVE" -C "$(dirname "$DOC_SOURCE")" "$(basename "$DOC_SOURCE")"
 if [ $? -ne 0 ]; then
         log "Erreur lors de la création de l'archive"
@@ -32,7 +32,7 @@ else
         log "Archive créée : $DOC_ARCHIVE"
 fi
 
-# Chiffrement de l'archive 
+# ========== Chiffrement de l'archive ========== 
 echo "$GPG_PASSWORD" | gpg --batch --yes --passphrase-fd 0 -c "$DOC_ARCHIVE"
 if [ $? -ne 0 ]; then
 	log "Echec du chiffrement de l'archive"
@@ -40,12 +40,12 @@ if [ $? -ne 0 ]; then
 else
 	log "Archive chiffrée : $ARCHIVE_ENCRYPTED"
 
-# Supprimer l'ancien archive non sécurisée
+# ========== Supprimer l'ancien archive non sécurisée ==========
 	rm -f "$DOC_ARCHIVE"
 	chmod 600 "$ARCHIVE_ENCRYPTED"
 fi
 
-# Synchronisation des fichiers DOC_SOURCE vers S3 AWS
+# ========== Synchronisation des fichiers DOC_SOURCE vers S3 AWS ==========
 
 log "Début de la sychronisatin $DOC_SOURCE ---> $s3_BUCKET"
 aws s3 cp "$DOC_SOURCE/" "$s3_BUCKET/Dossier_Client" --recursive >> "$LOG_FILE" 2>&1
